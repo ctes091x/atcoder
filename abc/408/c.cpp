accaa55f230a538726727e68c289b342e0e2c914
ctes091x <ctes091x@mma.club.uec.ac.jp>
2025-06-03 17:49:50 +0900

    abc408c

--- abc/408/c.cpp
+++ abc/408/c.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main() {
-  int protection[1000001];
   int num_wall, num_cannon;
   cin >> num_wall >> num_cannon;
+  vector<int> protection(num_wall+1);
   int left, right;
-  for(int i=1; i<=num_cannon; i++){
+  for(int i=0; i<num_cannon; i++){
     cin >> left >> right;
+    left--;
     protection[left]++;
-    protection[right+1]--;
+    protection[right]--;
   }
-  for(int i=1; i<=num_wall; i++){
-    protection[i]+=protection[i-1];
+  for(int i=0; i<num_wall; i++){
+    protection[i+1]+=protection[i];
   }
   int min=num_cannon;
-  for(int search=1; search<=num_wall; search++){
+  for(int search=0; search<num_wall; search++){
     if(protection[search]<min) min=protection[search];
   }
   cout << min << endl;
